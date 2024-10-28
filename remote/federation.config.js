6e737e45fe3ff2f7a10b3eb7aa2478b855b830c6
muenala <kmmuenala@gmail.com>
2024-10-28 01:12:42 -0500

    Initial setup: monorepo structure

---
+++ remote/federation.config.js
@@ -0,0 +1,25 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  name: 'remote',
+
+  exposes: {
+    './FeaturesModule': './src/app/features/features.module.ts',
+  }, 
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: false }),  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+  
+});
